324b2e71d4423bab5c68186c27176fe9296d8ad2
Mariam0422 <mariam79.yan@gmail.com>
2024-04-20 16:13:48 +0400

    add Const.js and blur

---
+++ script/const.js
@@ -0,0 +1,25 @@
+
+const API_URL = "https://reqres.in/api/users?page=1";
+const table = document.getElementById("table");
+const btn = document.getElementById("btn");
+const windowDiv = document.getElementById("window");
+const btnClosed = document.getElementById("closed");
+const createBtn = document.getElementById("createBtn");
+const firstNameInput = document.getElementById("firstName");
+const lastNameInput = document.getElementById("lastName");
+const emailInput = document.getElementById("email");
+const root = document.getElementById('root');
+const form = document.getElementById('form')
+export{
+    API_URL,
+    table,
+    btn,
+    windowDiv,
+    btnClosed,
+    createBtn,
+    firstNameInput,
+    lastNameInput,
+    emailInput,
+    root, 
+    form
+}
--- script/index.js
+++ script/index.js
@@ -1,23 +1,17 @@
-const API_URL = "https://reqres.in/api/users?page=1";
+import { API_URL,table,btn,windowDiv,btnClosed,createBtn,firstNameInput,lastNameInput,emailInput,root , form} from "./const.js";
 
-const table = document.getElementById("table");
-const btn = document.getElementById("btn");
-const windowDiv = document.getElementById("window");
-const btnClosed = document.getElementById("closed");
-const createBtn = document.getElementById("createBtn");
-const firstNameInput = document.getElementById("firstName");
-const lastNameInput = document.getElementById("lastName");
-const emailInput = document.getElementById("email");
 
 btn.addEventListener("click", ((e) => {
   e.preventDefault();
 windowDiv.style.display = "block";
+root.style.filter = 'blur(3px)'; 
 }));
 
 
 btnClosed.addEventListener("click", (e) => {
   e.preventDefault();
   windowDiv.style.display = "none";
+  root.style.filter = "";
 });
 
 

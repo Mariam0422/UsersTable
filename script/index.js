06f522c7ff7b52043d3be12068c71b8dedb9ac6d
Mariam0422 <mariam79.yan@gmail.com>
2024-04-20 15:22:20 +0400

    add User create

--- script/index.js
+++ script/index.js
@@ -1,6 +1,48 @@
-const API_URL = "https://reqres.in/api/users";
+const API_URL = "https://reqres.in/api/users?page=1";
 
 const table = document.getElementById("table");
+const btn = document.getElementById("btn");
+const windowDiv = document.getElementById("window");
+const btnClosed = document.getElementById("closed");
+const createBtn = document.getElementById("createBtn");
+const firstNameInput = document.getElementById("firstName");
+const lastNameInput = document.getElementById("lastName");
+const emailInput = document.getElementById("email");
+
+btn.addEventListener("click", ((e) => {
+  e.preventDefault();
+windowDiv.style.display = "block";
+}));
+
+
+btnClosed.addEventListener("click", (e) => {
+  e.preventDefault();
+  windowDiv.style.display = "none";
+});
+
+
+createBtn.addEventListener("submit", ((e) => {
+e.preventDefault();
+const userData = {
+ [firstNameInput.name] : firstNameInput.value,
+ [lastNameInput.name] : lastNameInput.value,
+ [emailInput.name] : emailInput.value
+};
+fetch(API_URL, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(userData)
+})
+.then((resp) => {
+  return resp.json();
+})
+.then((data) => {
+  console.log(data);
+});
+}));
+
 
 fetch(API_URL)
   .then((resp) => {
